746a63d0e8a70d1151e9421e066e84bd6e7a561d
Elena-Leonova <lena.postrash@gmail.com>
2023-06-08 20:57:48 +0300

    PhoneBook

---
+++ src/test/java/loginTest.java
@@ -0,0 +1,43 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class loginTest {
+    WebDriver wd;
+    @BeforeMethod
+    public void init(){
+    wd = new ChromeDriver();
+    wd.navigate().to("https://telranedu.web.app/home");
+    }
+    @Test
+    public void loginPositiveTest(){
+        // open login form
+    wd.findElement(By.xpath("//*[text()='LOGIN']")).click();
+        // fill login form
+        WebElement emailInput = wd.findElement(By.xpath("//input[1]"));
+        emailInput.click();
+        emailInput.clear();
+        emailInput.sendKeys("lena.postrash@gmail.com");
+        WebElement passwInput = wd.findElement(By.xpath("//input[2]"));
+        passwInput.click();
+        passwInput.clear();
+        passwInput.sendKeys("Mynameislena1!");
+        // click on login button
+        wd.findElement(By.xpath("//button[1]")).click();
+
+        // Assert
+        Assert.assertTrue(wd.findElements(By.xpath("//*[text()='Sign out']")).size() > 0);
+
+    }
+
+    @AfterMethod
+    public void tearDown(){
+       // wd.quit();
+    }
+
+}
